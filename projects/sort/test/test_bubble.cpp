0c30fd59fa5eb8eb82d0a598cb8ac42e603d9997
unknown <durnev075@example.com>
2025-12-15 01:45:38 +0300

    Create dz with cmake cpp_compilations

--- projects/sort/test/test_bubble.cpp
+++ projects/sort/test/test_bubble.cpp
@@ -1,14 +1,11 @@
+#include <gtest/gtest.h>
 #include "bubble_sort.hpp"
 #include <vector>
-#include <iostream>
 
-int main()
+TEST(BubbleSortTest, SortsCorrectly)
 {
     std::vector<int> v = {4, 2, 3, 1};
     bubbleSort(v);
-
-    for (int x : v)
-        std::cout << x << " ";
-
-    return 0;
+    std::vector<int> expected = {1, 2, 3, 4};
+    EXPECT_EQ(v, expected);
 }
